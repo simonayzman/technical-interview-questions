f223c91d8c0f509dfe290bbd809d7aa88ad8ed3f
Simon Ayzman <simonayzman@gmail.com>
2018-10-14 16:49:45 -0700

    Updated book index generator solution

--- (INPROGRESS) BookIndexGenerator/solution.cpp
+++
@@ -1,3 +0,0 @@
-// Return an (ordered) map, mapping words to a linked list or vector of integers, representing pages.
-// Pitfall 1: The same word can exist multiple times on a page.
-// TODO: Solution
